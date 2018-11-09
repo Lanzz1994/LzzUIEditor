5393700a0ceefe89439ebc8489cf6b38d11859c0
梁自在 <lzz@strongsoft.net>
2018-11-09 14:52:13 +0800

    a file of config with name ending with '.lzzui.js'

---
+++ src/pages/format.lzzui.js
@@ -0,0 +1,51 @@
+
+	var lzzjson = {
+		"Page": "",
+		"Desc": "",
+		"File": "",
+		"UIEditor": {
+
+			//UI树编辑
+			"UITreeEditor": "",
+
+			//模型编辑
+			"ModelEditor": "",
+
+			//样式编辑
+			"StyleEditor": "",
+
+			//属性编辑
+			"PropEditor": "",
+
+			//动效编辑
+			"MotionEditor": "",
+
+			//撤销恢复相关操作
+			"UndoEditor": "",
+
+			//快照预览：建立快照，可以操作多个版本进行预览
+			"PreviewEditor": "",
+
+			//布局编辑
+			"LayoutEditor": "",
+
+			//事件交互编辑
+			"EventEditor": "",
+
+		},
+		"UIToolEditor": {
+
+			//左侧菜单部件
+			"PartEditor": "",
+
+			//标尺工具
+			"Grid": "",
+
+
+			//模板编辑器
+			"TempleteEditor": "",
+
+
+		}
+	};
+	//专属 uieditor 的js文件，解析js的到可视化编辑器，
